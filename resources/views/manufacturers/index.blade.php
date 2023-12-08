e48a8c394ff068b8334acebc78d855dea3ffee5f
Ainsley Penza <ainsleypenza@hotmail.com>
2023-12-08 15:11:01 +0000

    Added templates for index of cars and manufacturers (with modifications)

--- resources/views/manufacturers/index.blade.php
+++ resources/views/manufacturers/index.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.main')
 
 @section('page')
-<h1>Manufacturers</h1>
+<main class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+                <div class="card-header card-title">
+                  <div class="d-flex align-items-center">
+                    <h2 class="mb-0">All Manufacturers</h2>
+                  </div>
+                </div>
+                <table class="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Address</th>
+                      <th scope="col">Phone</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Alfred</td>
+                      <td>Kuhlman</td>
+                      <td>alfred@test.com</td>
+                    </tr>
+                  </tbody>
+                </table> 
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
 @endsection
