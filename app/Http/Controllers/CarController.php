8f8704b22df151e69d3f1fda3957d739cabbc78b
Ainsley Penza <ainsleypenza@hotmail.com>
2023-12-08 15:00:15 +0000

    Added Controllers that respond to routers

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    //
+    public function index(){
+        return view('cars.index');
+    }
+    
+    public function details($id){
+        $car = Car::find($id);
+        return view('cars.details', compact('car'));
+    }
 }
--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ManufacturerController extends Controller
 {
-    //
+    public function index(){
+        return view('manufacturers.index');
+    }
 }
